169e00f5145dd8670c6183d9e5d70b36c91045fe
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-11-25 13:56:22 -0300

    update

---
+++ src/main/java/com/digisphere/CustomCraftHub/CustomCraftHubApplication.java
@@ -0,0 +1,13 @@
+package com.digisphere.CustomCraftHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCraftHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCraftHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digisphere/CustomCraftHub/CustomCraftHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digisphere.CustomCraftHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCraftHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
